de236b5728bdff6c78b3d65ca472fd72a2e67a04
shmaltalk <tmassachi@gmail.com>
2017-12-23 13:44:51 -0500

    updated to account for images in any location

--- loop.py
+++ loop.py
@@ -1,5 +1,13 @@
-import glob
 import subprocess
 
-for f in glob.glob('testimages/*'):
-    subprocess.call(['python3', 'demo.py', f])
+
+lists_f = open('data/train.txt', 'r')
+containing_path = '/home/taliem/ColorData/flowers/64x64/'
+
+for img in lists_f:
+    img = img.strip()
+    full_path = containing_path + img
+    subprocess.call(['python3','demo.py', './demo.jpg'])
+
+#for f in glob.glob('testimages/*'):
+#    subprocess.call(['python3', 'demo.py', f])
--- tools/create_image_lists.py
+++ tools/create_image_lists.py
@@ -4,13 +4,13 @@ import os
 f = open('data/train.txt', 'w')
 basepath = './flowers/'
 for p1 in os.listdir(basepath):
-    image = os.path.abspath(basepath + p1)
-    f.write(image + '\n')
+    #image = os.path.abspath(basepath + p1)
+    f.write(p1 + '\n')
 f.close()
 
 g = open('data/test.txt', 'w')
 basepath = './testimages/'
 for p in os.listdir(basepath):
-    image = os.path.abspath(basepath + p)
-    g.write(image + '\n')
+    #image = os.path.abspath(basepath + p)
+    g.write(p + '\n')
 g.close()
